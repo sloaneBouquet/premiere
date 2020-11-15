91eec271f0839b77fbe386907550e20e32952a88
sloaneBouquet <70756296+sloaneBouquet@users.noreply.github.com>
2020-11-15 19:08:45 +0100

    Add files via upload

---
+++ TD tim berners lee.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title></title>
+  </head>
+  <body>
+<h1> BIOGRAPHIE </h1>
+<p>Timothy John Berners-Lee, né le 8 juin 1955 à Londres,
+est un informaticien britannique, principal inventeur
+du World Wide Web (WWW) au tournant des années 1990 lors de
+ses travaux au CERN. En juillet 2004, il est fait chevalier
+par la reine Élisabeth II pour ce travail et son nom officiel devient
+Sir Timothy John Berners-Lee. Depuis 1994,
+il préside le World Wide Web Consortium (W3C), organismequ il a fondé.
+Il est lauréat du prix Turing 20161.
+  </body>
+</html>
